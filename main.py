ba5be34b7020a550f807bd7eaafe2ff583c567e9
Олег Пащенко <opashenko@stat4market.com>
2022-05-11 20:44:45 +0300

    version 3 (11.05.22

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt  # pip install matplotlib
+import math
 
 
 def read_file(file: str):
@@ -13,11 +14,33 @@ def read_file(file: str):
         try:
             f[i] = list(map(int, f[i].split(',')))
             if len(f[i]) != 3:
-                exit(0)
+                continue
             res.append(f[i])
         except Exception as error:
             print(f"В файле {file} содержится неккоректная строка под номером {i + 1}!\nПодробное описание ошибки: {error}")
     return res
+
+
+def to_write(filename, value, mode='a'):
+    with open(filename, mode, encoding='UTF-8') as f:
+        f.write(str(value))
+        f.write('\n')
+    f.close()
+
+
+def build_triple_graphic(data):
+    line_x, line_y, line_z = [], [], []
+    indx = 0
+    for d in data['data']:
+        indx += 0.1
+        line_x.append((d[0] - data['data'][0][0]) / indx)
+        line_y.append((d[1] - data['data'][0][1]) / indx)
+        line_z.append((d[2] - data['data'][0][2]) / indx)
+    figure = plt.figure()
+    s4 = figure.add_subplot(projection='3d')
+    s4.plot(line_x, line_y, line_z)
+    plt.title(f"Акселерометр")
+    plt.show()
 
 
 def build_solo_graphic(data, coordinate):
@@ -27,28 +50,49 @@ def build_solo_graphic(data, coordinate):
         index = 1
     else:
         index = 2
-
+    to_write(data['filename'].replace('.txt', '-x.txt'), '', 'w')
+    to_write(data['filename'].replace('.txt', '-y.txt'), '', 'w')
+    to_write(data['filename'].replace('.txt', '-z.txt'), '', 'w')
     figure = plt.figure()
     s4 = figure.add_subplot()
 
     arr_x, arr_y = [], []
+    x_arr, y_arr, z_arr = [], [], []
     i = 0
     for d in data['data']:
         i += 0.1
+        if '.0' in str(i):  # analyze every one second
+            to_write(data['filename'].replace('.txt', '-x.txt'), d[0])
+            to_write(data['filename'].replace('.txt', '-y.txt'), d[1])
+            to_write(data['filename'].replace('.txt', '-z.txt'), d[2])
+        x_arr.append(d[0])
+        y_arr.append(d[1])
+        z_arr.append(d[2])
         arr_x.append(i)
         arr_y.append(d[index])
     s4.plot(arr_x, arr_y)
-    plt.title(f"График функции {data['filename']} координаты {coordinate} по времени.")
+    plt.title(f"{coordinate}, Summary SKO: X-{srqrt_otkl(x_arr)}, Y-{srqrt_otkl(y_arr)}, Z-{srqrt_otkl(z_arr)}")
     plt.show()
 
 
+def srqrt_otkl(sp):
+    res = 0
+    for ii in sp:
+        res += (ii - (sum(sp) / len(sp))) ** 2
+    res /= len(sp)
+    return math.sqrt(res)
+
+
 def main():
-    case_filenames = ['/Users/olegpash/PycharmProjects/tema/tests/14,45,56-Даниил, левая-workPC.txt',
-                      '/Users/olegpash/PycharmProjects/tema/tests/14,48,33-Даниил, левая-Sport.txt']
+
+    case_filenames = [
+        '/Users/olegpash/PycharmProjects/tema/tests/14,45,56-Даниил, левая-workPC.txt',
+        '/Users/olegpash/PycharmProjects/tema/tests/14,48,33-Даниил, левая-Sport.txt']
     cases_data = []
     for filename in case_filenames:
         cases_data.append({'data': read_file(filename), 'filename': filename})
     for case in cases_data:
+        build_triple_graphic(case)
         build_solo_graphic(case, 'X')
         build_solo_graphic(case, 'Y')
         build_solo_graphic(case, 'Z')
@@ -56,4 +100,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
