76b82d70941288e8bcd23fe8144bcc9859b0720d
Олег Пащенко <opashenko@stat4market.com>
2022-05-15 21:07:34 +0300

    version 4 (final)

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ def read_file(file: str):
 
 def to_write(filename, value, mode='a'):
     with open(filename, mode, encoding='UTF-8') as f:
+        if mode == 'w':
+            f.close()
+            return
         f.write(str(value))
         f.write('\n')
     f.close()
@@ -53,6 +56,7 @@ def build_solo_graphic(data, coordinate):
     to_write(data['filename'].replace('.txt', '-x.txt'), '', 'w')
     to_write(data['filename'].replace('.txt', '-y.txt'), '', 'w')
     to_write(data['filename'].replace('.txt', '-z.txt'), '', 'w')
+    to_write(data['filename'].replace('.txt', '-xyz.txt'), '', 'w')
     figure = plt.figure()
     s4 = figure.add_subplot()
 
@@ -65,6 +69,7 @@ def build_solo_graphic(data, coordinate):
             to_write(data['filename'].replace('.txt', '-x.txt'), d[0])
             to_write(data['filename'].replace('.txt', '-y.txt'), d[1])
             to_write(data['filename'].replace('.txt', '-z.txt'), d[2])
+            to_write(data['filename'].replace('.txt', '-xyz.txt'), math.sqrt(d[0] ** 2 + d[1] ** 2 + d[2] ** 2))
         x_arr.append(d[0])
         y_arr.append(d[1])
         z_arr.append(d[2])
@@ -83,11 +88,34 @@ def srqrt_otkl(sp):
     return math.sqrt(res)
 
 
-def main():
+def simple_read_file(filename: str):
+    return open(filename, mode='r', encoding="UTF-8").read().split('\n')[:-1]
 
+
+def linear_deviation(arr: list, file_to_check: str):
+    array_to_check = simple_read_file(file_to_check.replace('.txt', '-xyz.txt'))
+    result_arr = []
+    for i in range(len(arr) - 1):
+        to_write(arr[i]['filename'].replace('.txt', '-ld.txt'), '', 'w')
+        array_to_minus = simple_read_file(arr[i]['filename'].replace('.txt', '-xyz.txt'))
+        result = 0
+        for j in range(len(array_to_check)):
+            try:
+                to_write(arr[i]['filename'].replace('.txt', '-ld.txt'), float(array_to_check[j]) - float(array_to_minus[j]), 'a')
+                result += float(array_to_check[j]) - float(array_to_minus[j])
+            except:
+                pass
+        result_arr.append([result, arr[i]['filename'].replace('.txt', '-ld.txt')])
+    print(result_arr)
+    result_arr.sort()  # reverse=True
+    print(result_arr[0][1])
+
+def main():
+    file_to_check = '/Users/olegpash/PycharmProjects/tema/tests/16_52_0-Daniil_levaya-Sit.txt'
     case_filenames = [
         '/Users/olegpash/PycharmProjects/tema/tests/14,45,56-Даниил, левая-workPC.txt',
-        '/Users/olegpash/PycharmProjects/tema/tests/14,48,33-Даниил, левая-Sport.txt']
+        '/Users/olegpash/PycharmProjects/tema/tests/14,48,33-Даниил, левая-Sport.txt',
+        file_to_check]
     cases_data = []
     for filename in case_filenames:
         cases_data.append({'data': read_file(filename), 'filename': filename})
@@ -96,6 +124,9 @@ def main():
         build_solo_graphic(case, 'X')
         build_solo_graphic(case, 'Y')
         build_solo_graphic(case, 'Z')
+    linear_deviation(cases_data, file_to_check)
+
+
 
 
 if __name__ == "__main__":
